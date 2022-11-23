754732592937658ebd8d04babf9ac168a4cf1323
Ezh+Wine <valvic90@gmail.com>
2022-11-23 10:28:32 +0300

    initial commit

---
+++ homework3.js
@@ -0,0 +1,59 @@
+// ДЗ
+
+// Создайте функцию которая возводит переданное число в куб, необходимо вывести в консоль результат 2^3 степени + 3 ^ 3 степени
+
+// const tripleNumber = number => number ** 3;
+
+// console.log(tripleNumber(2) + tripleNumber(3));
+
+
+// Пользователь вводит с клавиатуры число, если ввёл текст, необходимо вывести что значение задано неверно
+// Создать фукнцию, которая высчитывает 13 % от данного числа и выводит в консоль текст "Размер заработной 
+// платы за вычетом налогов равен значение"
+
+// function countPaycheck(salary) {
+//     let paycheck = salary * 0.87;
+//     console.log(`Размер заработной платы за вычетом налогов равен ${paycheck}`);
+// }
+
+// let userSalary = Number(prompt('Введите сумму вашей зарпаты'));
+
+// (isNaN(userSalary)) ? console.log('Значение задано неверно') : countPaycheck(userSalary);
+
+
+// Пользователь с клавиатуры вводит 3 числа, необходимо создать функцию, 
+// которая определяет максимальное значение среди этих чисел
+
+// function findMaximum(num1, num2, num3) {
+//     let max = num1;
+//     if (num1 < num2) max = num2;
+//     if (num3 > max) max = num3;
+//     return max;
+// }
+
+// ИЛИ
+
+// function findMaximum(num1, num2, num3) {
+//     if (num1 >= num2 && num2 >= num3) return num1;
+//     else if (num1 <= num2 && num2 >= num3) return num2;
+//     else if (num1 <= num2 && num2 <= num3) return num3;
+// }
+
+
+// let firstUserNumber = Number(prompt('Введите первое число'));
+// let secondUserNumber = Number(prompt('Введите второе число'));
+// let thirdUserNumber = Number(prompt('Введите тьерте число'));
+
+// alert(`Максимальное число ${findMaximum(firstUserNumber, secondUserNumber, thirdUserNumber)}`);
+
+// Задание 4
+
+let sumNumbers = (firstNum, secondNum) => firstNum + secondNum;
+let diffNumbers = (firstNum, secondNum) => (firstNum >= secondNum) ? firstNum - secondNum : secondNum - firstNum;
+let prodNumbers = (firstNum, secondNum) => firstNum * secondNum;
+let quotNumbers = (firstNum, secondNum) => (firstNum >= secondNum) ? firstNum / secondNum : secondNum / firstNum;
+
+console.log(sumNumbers(3, 5));
+console.log(diffNumbers(3, 5));
+console.log(prodNumbers(3, 5));
+console.log(quotNumbers(3, 5));
